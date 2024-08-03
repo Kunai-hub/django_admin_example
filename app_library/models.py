caa9e866f30ab752e7336b42b4bcec59cfe0fee0
kunai <neni23@mail.ru>
2024-08-03 16:01:04 +0300

    added model Book

--- app_library/models.py
+++ app_library/models.py
@@ -5,8 +5,11 @@ class Publisher(models.Model):
     name = models.CharField(max_length=30)
     genre = models.CharField(max_length=30)
     city = models.CharField(max_length=60)
-    county = models.CharField(max_length=60)
+    country = models.CharField(max_length=60)
     is_active = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.id}. {self.name} ({self.country}, {self.city})'
 
 
 class Author(models.Model):
@@ -14,3 +17,22 @@ class Author(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
     biography = models.TextField()
+    second_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=16, blank=True)
+    personal_page = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    city = models.CharField(max_length=60, blank=True)
+    country = models.CharField(max_length=60, blank=True)
+    university = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.first_name} {self.last_name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateField()
