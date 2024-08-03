49b86cd478526ebb8d59e7d84c77da9027a48f3d
kunai <neni23@mail.ru>
2024-08-03 14:17:19 +0300

    added model Author

--- app_library/models.py
+++ app_library/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
-    genre = models.CharField(max_length=50)
+    genre = models.CharField(max_length=30)
     city = models.CharField(max_length=60)
     county = models.CharField(max_length=60)
     is_active = models.BooleanField()
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    biography = models.TextField()
