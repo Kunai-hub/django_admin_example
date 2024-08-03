1bd1ffc39904c8770afec85b339f8aacc545fcc8
kunai <neni23@mail.ru>
2024-08-03 14:17:34 +0300

    added search for models

--- app_library/admin.py
+++ app_library/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from app_library.models import Publisher
+from app_library.models import Publisher, Author
 
 
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'city']
+    list_filter = ['is_active', 'city']
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name']
+    search_fields = ['first_name', 'last_name', 'biography']
 
 
 admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Author, AuthorAdmin)
