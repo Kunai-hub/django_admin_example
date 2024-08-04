1915f6b5ac9fdd71f06a1eccc6cf5228245f181f
kunai <neni23@mail.ru>
2024-08-04 11:51:23 +0300

    init commit for application 'app_users'

--- django_admin_example/settings.py
+++ django_admin_example/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "app_library.apps.AppLibraryConfig"
+    "app_library.apps.AppLibraryConfig",
+    "app_users.apps.AppUsersConfig"
 ]
 
 MIDDLEWARE = [
