38b05e44b2db9b5293bf21e4a34bc23881a39d74
kunai <neni23@mail.ru>
2024-08-04 15:39:11 +0300

    added logout

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app_users.views import login_view, AnotherLoginView
+from app_users.views import login_view, AnotherLoginView, logout_view, AnotherLogoutView
 
 urlpatterns = [
     path('login/', login_view, name='login_view'),
-    path('another_login/', AnotherLoginView.as_view(), name='another_login')
+    path('logout/', logout_view, name='logout_view'),
+    path('another_login/', AnotherLoginView.as_view(), name='another_login'),
+    path('another_logout/', AnotherLogoutView.as_view(), name='another_logout'),
 ]
--- app_users/views.py
+++ app_users/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
 from app_users.forms import AuthForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 def login_view(request):
@@ -37,3 +37,13 @@ def login_view(request):
 
 class AnotherLoginView(LoginView):
     template_name = 'users/login.html'
+    # next_page = '/'
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse('Вы успешно вышли из системы!')
+
+
+class AnotherLogoutView(LogoutView):
+    template_name = 'users/logout.html'
