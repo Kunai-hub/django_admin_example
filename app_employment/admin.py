3f8e5295497f6be3f3a8ae48d315d20074caa17b
kunai <neni23@mail.ru>
2024-08-05 16:41:58 +0300

    added new group and permissions

--- app_employment/admin.py
+++ app_employment/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from app_employment.models import Vacancy
+from app_employment.models import Vacancy, Resume
 
 
 class VacancyAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', ]
 
 
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title',]
+
+
 admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Resume, ResumeAdmin)
--- app_employment/models.py
+++ app_employment/models.py
@@ -18,3 +18,19 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Resume(models.Model):
+    is_active = models.BooleanField(default=False, verbose_name='Активность')
+    title = models.CharField(max_length=30, verbose_name='Заголовок')
+    description = models.TextField(default='', verbose_name='Описание')
+    publisher = models.CharField(max_length=30, verbose_name='Кто опубликовал')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    published_at = models.DateTimeField(verbose_name='Дата публикации', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Резюме'
+        verbose_name_plural = 'Резюме'
+
+    def __str__(self):
+        return self.title
