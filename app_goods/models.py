69d0bc971eaa0e935767e242dfd96a7a35a960e7
kunai <neni23@mail.ru>
2024-08-07 13:33:57 +0300

    i18n for admin page

--- app_goods/models.py
+++ app_goods/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Item(models.Model):
-    code = models.CharField(max_length=100, verbose_name='Артикул')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    code = models.CharField(max_length=100, verbose_name=_('Артикул'))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Цена'))
+
+    class Meta:
+        verbose_name_plural = _('товары')
+        verbose_name = _('товар')
